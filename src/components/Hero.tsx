a61e6448c379f84d3f44be162dea0a938ca896ff
Lanz Alexander Malto <lmalto382@gmail.com>
2025-11-03 23:38:24 +0800

    edits to responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import image from "../assets/mypic.png"
 
 const Hero = () => {
     return (
-        <section className = "pt-28 flex flex-col md:flex-row justify-center items-center px-10 w-full md:px-16 gap-8">
+        <section className = "min-h-screen pt-28 pb-12 md:pt-20 md:pb-0 flex flex-col md:flex-row justify-center items-center px-10 w-full md:px-16 gap-8">
             <div className = "w-full md:w-1/2">
                 <p className="mb-3 text-sm font-semibold text-amber-600">Developer, Data Geek, Mathematician.</p>
                 <h1 className="text-4xl font-bold text-neutral-900 dark:text-white md:text-5xl">
@@ -21,11 +21,16 @@ const Hero = () => {
                 </div>
             </div>
             <div className = "w-full md:w-1/2">
-                <div className="h-100 w-full rounded-2xl bg-gradient-to-tr from-amber-300 via-rose-300 to-sky-300 p-1 dark:from-amber-400 dark:via-rose-400 dark:to-sky-400">
+                {/* <div className="h-[90%] w-[90%] md:h-[75%] md:w-[75%] rounded-2xl bg-gradient-to-tr from-amber-300 via-rose-300 to-sky-300 p-1 dark:from-amber-400 dark:via-rose-400 dark:to-sky-400">
                     <div className="flex h-full w-full items-center justify-center rounded-2xl bg-white/80 backdrop-blur dark:bg-neutral-900/70">
-                        <span className="text-5xl">👋</span>
+                        <img src = {image} style={{width: '100%', height: 'auto'}}/>
                     </div>
-                </div>
+                </div> */}
+                <div className="h-100 w-full rounded-2xl bg-gradient-to-tr from-amber-300 via-rose-300 to-sky-300 p-1 dark:from-amber-400 dark:via-rose-400 dark:to-sky-400">
+              <div className="flex h-full w-full items-center justify-center rounded-2xl bg-white/80 backdrop-blur dark:bg-neutral-900/70">
+                <span className="text-5xl">👋</span>
+              </div>
+            </div>  
             </div>
         </section>
     )
